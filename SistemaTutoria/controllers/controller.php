af58672304013f0bc85c66f146d7420085adf198
Aaronfor <33106153+Aaronfor@users.noreply.github.com>
2018-05-24 22:56:13 -0500

    practica08

---
+++ SistemaTutoria/controllers/controller.php
@@ -0,0 +1,43 @@
+<?php
+
+Class MvcController{
+
+
+	public function pagina(){	
+		
+		include "views/template.php";
+	
+	}
+
+	public function enlacesPaginasController(){
+
+		if(isset( $_GET['action'])){
+			
+			$enlaces = $_GET['action'];
+		
+		}
+
+		else{
+
+			$enlaces = "index";
+		}
+
+		$respuesta = Paginas::enlacesPaginasModel($enlaces);
+
+		include $respuesta;
+
+	}
+
+	#IDENTIFICACION DE USUARIOS
+
+	public function ingresoUsuarioController(){
+
+		
+
+		
+	}
+
+
+}
+
+?>
---
+++ SistemaTutoria/models/enlaces.php
@@ -0,0 +1,16 @@
+<?php
+
+	class Paginas{
+		
+		public function enlacesPaginasModel($enlaces){
+
+			if($enlaces=="index" || $enlaces=="fallo"){
+				$module =  "views/modules/ingresar.php";
+			}else{
+				$module =  "views/modules/".$enlaces.".php";
+			}
+			return $module;
+
+		}
+	}
+?>
